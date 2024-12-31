1c71cd4d2d57546926b295bddda8b982dc1bcffd
sankalpverma <sankalp.verma@iphtechnologies.com>
2024-12-31 10:01:58 +0530

    Dependency override

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "DataMarketplacePackage",
+    name: "dataMarketplacePackage",
     platforms: [
         .iOS(.v13),
         .macOS(.v10_15)
