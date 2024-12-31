32ebf0e1e4a23cc91d210aae76f23398aece6128
sankalpverma <sankalp.verma@iphtechnologies.com>
2024-12-31 09:50:44 +0530

    Added package dependency

--- Package.swift
+++ Package.swift
@@ -15,7 +15,10 @@ let package = Package(
             targets: ["DataMarketplacePackage"]
         )
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/sankalp-verma-iphtech/DataMarketPlace", from: "1.0.0")
+
+    ],
     targets: [
         .target(
             name: "DataMarketplacePackage",
